cece847d47a7c4a8b800d118757b90fa3a5f77f1
nikitakot <kotn01@outlook.cz>
2018-12-12 23:32:10 +0100

    rename bindings

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from './prisma.binding';
+import { Prisma } from './prisma.bindings.generated';
 
 @Injectable()
 export class PrismaService extends Prisma {
