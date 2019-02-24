f8f4c9f96573331b18938c6fdc9b2ae9ce8f0a0e
nikitakot <kotn01@outlook.cz>
2019-02-24 23:46:30 +0100

    doing it live

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+  app.enableCors();
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
