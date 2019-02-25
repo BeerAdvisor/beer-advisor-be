3ecc15ab3f736f694c31a149c160bdbcc1da4d83
nikitakot <kotn01@outlook.cz>
2019-02-25 21:47:32 +0100

    add origins instead of *

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  app.enableCors({ credentials: true, origin: true, allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json' });
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://beer-advisor-development.herokuapp.com/'],
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT || 3000);
 }
