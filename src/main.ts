e6f03993466e3a008c2d604e2d6d414617a6bfb5
nikitakot <kotn01@outlook.cz>
2019-02-25 20:45:08 +0100

    enable cors credantials

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  app.enableCors();
+  app.enableCors({ credentials: true });
   app.use(cookieParser());
   await app.listen(process.env.PORT || 3000);
 }
